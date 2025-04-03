f9bb80cfdc478b78edcd5b10ce87432c4bb033e7
Adam Cahan <adam@codeplatoon.org>
2025-04-03 09:30:00 -0500

    cleanup

--- deployment-app/back-end/list_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from .views import All_lists, A_list, Lists_test
-
-urlpatterns = [
-    path('', All_lists.as_view()),
-    path('test/', Lists_test.as_view()),
-    path("<int:list_id>/", A_list.as_view()),
-    path("<int:list_id>/tasks/", include("task_app.urls")),
-]
--- deployment-app/back-end/task_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import All_tasks
-
-urlpatterns = [
-    path("", All_tasks.as_view()),
-]
